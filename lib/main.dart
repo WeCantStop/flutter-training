3090c50bc28d2c592beaff41e4b727981d2e192a
panwei <panwei@zhongan.com>
2018-12-11 11:12:52 +0800

    :art: update

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,84 @@
 import 'package:flutter/material.dart';
 
-//主函数（入口函数），下面我会简单说说Dart的函数
 void main() => runApp(MyApp());
 
-// 生命Home widget
+// 声明 Home Widget
 class BodyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Container(
-      child: new Column(
-        children: <Widget>[
-          new RaisedButton(
-            onPressed: () {
-              print('点击按钮一了');
-            },
-            color: Colors.green,
-            child: Text('按钮一', style: TextStyle(color: Colors.pink),),
-          ),
-          new Text('二'),
-          new Text('三'),
-          new Text('四')
-        ],
-      ),
-      padding: EdgeInsets.fromLTRB(20, 20, 0, 0));
+        child: new Column(
+          children: <Widget>[
+            new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: new RaisedButton(
+                    onPressed: () {
+                      print('点击按钮一了');
+                    },
+                    color: Colors.green,
+                    child: Text(
+                      '按钮一',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            new Row(
+              children: <Widget>[
+                new Expanded(
+                    child: new RaisedButton(
+                  onPressed: () {
+                    print('点击了左一');
+                  },
+                  child: new Text('左一', textAlign: TextAlign.center),
+                  color: Colors.blue,
+                )),
+                new Expanded(
+                  child: new Container(
+                    child: new Text('左二', textAlign: TextAlign.center),
+                    color: Colors.yellow,
+                    margin: EdgeInsets.only(left: 20),
+                  ),
+                )
+              ],
+            ),
+            new Container(
+              child: new Text(
+                '三奥术大师大大大所大撒大多三奥术大师大大大所大撒大多三奥术大师大大大所大撒大多三奥术大师大大大所大撒大多三奥术大师大大大所大撒大多三奥术大师大大大所大撒大多',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              margin: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(const Radius.circular(8.0))),
+            ),
+            new Image.network(
+              'https://tac-cdn.zhongan.com/wxapp/activity/double_99/top_bg.png'
+            )
+          ],
+        ),
+        padding: EdgeInsets.fromLTRB(10, 20, 20, 0),
+        color: Colors.black12,
+      );
 }
 
-// 声明MyApp类
+// 声明 首页 Widget
 class MyApp extends StatelessWidget {
   //重写build方法
   @override
   Widget build(BuildContext context) {
     //返回一个Material风格的组件
     return MaterialApp(
-        title: 'Welcome to Flutteraa',
+        title: 'Home',
         home: Scaffold(
-            //创建一个Bar，并添加文本
             appBar: AppBar(
               title: Text(
                 '首页',
